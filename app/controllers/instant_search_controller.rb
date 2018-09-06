0025b272db17fa43e8a847535b4d4f4b89f5ef07
Armin <adajic1@etf.unsa.ba>
2018-09-06 16:49:45 +0200

    Retrieving articles

--- app/controllers/instant_search_controller.rb
+++ app/controllers/instant_search_controller.rb
@@ -8,8 +8,12 @@ class InstantSearchController < ApplicationController
   # New search query
   def create
     query_string = params[:body] 
-    IncomingSearchQueryService.call(query_string, request.remote_ip)       
-    send_data query_string
+    IncomingSearchQueryService.call(query_string, request.remote_ip)   
+    @articles = Article.get_relevant_articles(query_string) 
+    return_data = {}
+    return_data["articles"] = @articles
+    return_data["query"] = query_string
+    send_data return_data.to_json
   end
   
 end
