2ab5dd3e3fea88951e7701b7ca10b34a116a4fa6
Armin <adajic1@etf.unsa.ba>
2018-09-06 21:54:45 +0200

    Selecting article for reading update

--- app/assets/javascripts/instant_search.js
+++ app/assets/javascripts/instant_search.js
@@ -23,19 +23,20 @@ $(document).ready(function() {
 		        data: { body: new_value, authenticity_token: $('[name="csrf-token"]')[0].content},
 		        async: true,
 		        success: function(result) {
-					response_object = JSON.parse(result);				
-					response_query = response_object.query;	
+					var response_object = JSON.parse(result);				
+					var response_query = response_object.query;	
 					if (response_query.length==0) response_query="-empty-";			
 					$('#result').html("<b>Response:</b> "+response_query);
 					$('#result').css("visibility","visible");
-					// Now show articles
 					$('#articles_list').html("");
 					$('#article').html("");
 					for (i = 0; i < response_object.articles.length; i++) {
-						articleTemplateInsert("#articles_list", 
-											  response_object.articles[i].id, 
-											  response_object.articles[i].description, 
-											  response_object.articles[i].content);
+						var id = response_object.articles[i].id;
+						var description = response_object.articles[i].description;
+						var shortened_content = "...";
+						var full_content = response_object.articles[i].content;
+						articleTemplateInsert("#articles_list", id, description, shortened_content);
+						$('#article_full_content_'+id).html(full_content);
 					}
 		        },
 		        error: function(result) {
@@ -91,14 +92,15 @@ $(document).ready(function() {
 
 	$(document).on('page:load', $('#searchbox').val(''));
 	
-	$(document).on('click', '[id^="instant_search_article_span_"]', function() {
+	$(document).on('click', '[id^="article_span_instant_search_"]', function() {
 		// id attribute is expected to be of the following format: instant_search_article_span_<id>
     	var words = $(this).attr("id").split("_");
 		var id = words[words.length - 1];
+		if (id.startsWith("preview")) return;
 		var description = $('#article_description_'+id).html();
-		var content = $('#article_content_'+id).html();
+		var content = $('#article_full_content_'+id).html();
 		$('#article').html("");
-		articleTemplateInsert("#article", id, description, content);
+		articleTemplateInsert("#article", "preview_"+id, description, content);
 	});
 	
 });
