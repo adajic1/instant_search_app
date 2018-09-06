ecd85b08a8245596661e15b800e66904ff80c314
Armin <adajic1@etf.unsa.ba>
2018-09-06 20:49:42 +0200

    Removed article closing action

--- app/models/user_action.rb
+++ app/models/user_action.rb
@@ -4,7 +4,6 @@ class UserAction < ApplicationRecord
   # Constant strings defining actions
   TYPE_SEARCH = "searched article"
   TYPE_READ = "read article"
-  TYPE_CLOSE_ARTICLE = "closed article"  
   TYPE_CLOSE_TAB = "closed tab"
   TYPE_SUPPORT = "contacted support" 
   
