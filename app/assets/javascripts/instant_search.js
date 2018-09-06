4cc5fe97aa2c361fc8b5bad49cf476d71731fcb3
Armin <adajic1@etf.unsa.ba>
2018-09-06 22:58:41 +0200

    Increased shortened content to 150 characters

--- app/assets/javascripts/instant_search.js
+++ app/assets/javascripts/instant_search.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 	}  
     
     function getShortenedContent(content) {
-    	var num_chars = 50;
+    	var num_chars = 150;
     	if (content.length<=num_chars) 
     		return content;
     	else 
