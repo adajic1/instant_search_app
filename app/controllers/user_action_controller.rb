2de064085652af0c3145d1f3df5e168be3137e0a
Armin <adajic1@etf.unsa.ba>
2018-09-06 13:17:46 +0200

    Just added UserAction controller

---
+++ app/controllers/user_action_controller.rb
@@ -0,0 +1,2 @@
+class UserActionController < ApplicationController
+end
---
+++ test/controllers/user_action_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserActionControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
