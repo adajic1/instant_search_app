6cd19454cb9ad2bd1f20dc750815b157827ca63f
Armin <adajic1@etf.unsa.ba>
2018-09-06 15:11:52 +0200

    Search query is recorded as UsserAction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_06_125324) do
+ActiveRecord::Schema.define(version: 2018_09_06_095341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
