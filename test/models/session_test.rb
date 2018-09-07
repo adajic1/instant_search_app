388d388ebbd68a6158958b52fe0c183c549d65c4
Armin <adajic1@etf.unsa.ba>
2018-09-07 14:25:23 +0200

    Added more tests

--- test/models/session_test.rb
+++ test/models/session_test.rb
@@ -1,7 +1,30 @@
 require_relative "../test_helper"
 
 class SessionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    #  
+  end
+
+  def teardown
+    ## Nothing really
+  end
+
+  def test_get_or_create
+    ip_body = "127.255.255.255"
+    @session = Session.get_or_create(ip_body) 
+    assert_equal(ip_body, @session.body) # Test if it created session with given ip    
+    assert_equal(3, Session.all.count) # There should be 3 sessions now (2 from fixtures)
+  end
+  
+  def test_last_user_action_is_search?
+    ip_body = "127.255.255.255"
+    @session = Session.get_or_create(ip_body) 
+    assert_equal(false, @session.last_user_action_is_search?)  
+    
+    @old_query = ""
+    @new_query = "test" 
+    UserAction.register_type_search(@session, @old_query, @new_query) 
+    assert_equal(true, @session.last_user_action_is_search?) 
+  end
+   
 end
--- test/models/user_action_test.rb
+++ test/models/user_action_test.rb
@@ -1,7 +1,20 @@
 require_relative "../test_helper"
 
 class UserActionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    ip_body = "127.255.255.255"
+    @session = Session.get_or_create(ip_body)  
+  end
+
+  def teardown
+    ## Nothing really
+  end
+
+  def test_register_type_search
+    @old_query = ""
+    @new_query = "test" 
+    assert_equal(false, @session.last_user_action_is_search?) 
+    UserAction.register_type_search(@session, @old_query, @new_query) 
+    assert_equal(true, @session.last_user_action_is_search?) 
+  end
 end
