24667d7faec30446cdf4c8a4f1a8c5a3f3d01eff
Armin <adajic1@etf.unsa.ba>
2018-09-07 14:10:00 +0200

    Added test test_get_relevant_articles

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,17 @@
-require 'test_helper'
+require_relative "../test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    #   
+  end
+
+  def teardown
+    ## Nothing really
+  end
+
+  def test_get_relevant_articles
+    # Test if it retrieves fixtures (there are 2 articles there)
+    articles = Article.get_relevant_articles("something")
+    assert_equal(2, articles.count) 
+  end
 end
--- test/models/search_query_test.rb
+++ test/models/search_query_test.rb
@@ -1,7 +1,23 @@
-require 'test_helper'
+require_relative "../test_helper"
 
 class SearchQueryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @session = Session.get_or_create("127.255.255.255") 
+    # Note that query type depends on @session.user_actions.last, as well as @old_query, @new_query
+  end
+
+  def teardown
+    ## Nothing really
+  end
+
+  def test_get_relevant_articles
+    @old_query = ""
+    @new_query = "test" 
+    
+    # Test type of search query
+    query_type = SearchQuery.get_type_of_search_query(@session, @old_query, @new_query)    
+    assert_equal(SearchQuery::QUERY_TYPE_NEW, query_type)  
+    # This is considered QUERY_TYPE_NEW because last user action of this session is not a search.
+    # If it was and it fitted @old_query then it would be considered as QUERY_TYPE_UPDATE.
+  end
 end
